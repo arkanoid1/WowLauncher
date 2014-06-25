9dd20a0a6e5f8f06a38b72b925c7393d12d6ccb9
Felipe Apablaza <fapablaza@adexus.cl>
2014-06-24 21:04:02 -0400

    Agregadas clases de menues.

--- src/launcher/gui/Menu.cpp
+++ src/launcher/gui/Menu.cpp
@@ -1,20 +1,33 @@
 
 #include "Menu.hpp"
-
 #include "MenuItem.hpp"
 #include "MenuBar.hpp"
-#include <Windows.h>
+
+#include "../WindowsError.hpp"
 
 namespace gui {
 
 	int Menu::count = 0;
 
-	Menu::Menu(MenuBar *menuBar, const std::wstring &text) {
+    Menu::Menu(MenuBar *menuBar, const std::wstring &text) : menuHandler(NULL) {
+        if (menuBar == nullptr) {
+            throw std::runtime_error("Menu::Menu: Menubar pointer can't be null.");
+        }
 
+        MenuHandler menuHandler;
+
+        if (::AppendMenu(menuBar->getHandle(), MF_POPUP | MF_ENABLED | MF_STRING, (UINT_PTR)((HMENU)menuHandler), text.c_str()) == FALSE) {
+            throw WindowsError(::GetLastError());
+        }
+
+        Menu::count++;
+        this->menuHandler = menuHandler;
 	}
 
 	Menu::~Menu() {
-
+        if (this->hMenu) {
+            ::DestroyMenu(NULL);
+        }
 	}
 
 	MenuItem* Menu::addMenuItem(const std::wstring &text) {
--- src/launcher/gui/Menu.hpp
+++ src/launcher/gui/Menu.hpp
@@ -3,10 +3,42 @@
 #define __GUI_MENU_HPP__
 
 #include <string>
+#include <memory>
+#include <Windows.h>
 
 namespace gui {
 	class MenuItem;
 	class MenuBar;
+
+    struct MenuHandler {
+        HMENU hMenu;
+
+        MenuHandler() : hMenu(NULL) {
+            this->hMenu = ::CreateMenu();
+        }
+
+        MenuHandler(HMENU hMenu) : hMenu(NULL) {
+            this->hMenu = hMenu;
+        }
+
+        ~MenuHandler() {
+            if (this->hMenu) {
+                ::DestroyMenu(hMenu);
+            }
+        }
+
+        operator HMENU() const {
+            return hMenu;
+        }
+
+
+        MenuHandler& operator= (MenuHandler &other) {
+            this->hMenu = other.hMenu;
+            other.hMenu = NULL;
+
+            return *this;
+        }
+    };
 
 	class Menu {
 	protected:
@@ -16,11 +48,20 @@ namespace gui {
 	public:
 		MenuItem* addMenuItem(const std::wstring &text);
 
+        std::wstring getText() const;
+        void setText(std::wstring &text);
+
+        int getId() const;
+
 	private:
 		MenuBar *menuBar;
 
 	private:
 		static int count;
+        static const int base = 10000;
+
+    private:
+        MenuHandler menuHandler;
 	};
 }
 
